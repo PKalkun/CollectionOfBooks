222f48bb9169dbb9d3aa53f8866fca057933d525
PKalkun <pkalkun@gmail.com>
2018-12-23 11:39:44 +0100

    Code CleanUp

--- library/src/library/spring/dao/BookDAOImpl.java
+++ library/src/library/spring/dao/BookDAOImpl.java
@@ -5,12 +5,8 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import library.spring.controller.LibraryController;
 import library.spring.entity.Book;
 
 @Repository
